478e690003e2d66274c09e3602aa3fb2dbabbc6f
Amathech <amarachimiracle2704@gmail.com>
2026-04-26 13:06:42 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,14 @@
+// firebase.js
 
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getFirestore, doc, getDoc, setDoc, collection, addDoc } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "YOUR_KEY",
+  authDomain: "YOUR_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export { doc, getDoc, setDoc, collection, addDoc };
