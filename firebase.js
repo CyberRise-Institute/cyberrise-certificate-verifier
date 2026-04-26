6e0299aeaa7537211c7bf3dfd69b26ea88323f9a
Amathech <amarachimiracle2704@gmail.com>
2026-04-26 13:58:04 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,14 +1,14 @@
 // firebase.js
-
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getFirestore, doc, getDoc, setDoc, collection, addDoc } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-
 const firebaseConfig = {
-  apiKey: "YOUR_KEY",
-  authDomain: "YOUR_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
+  apiKey: "AIzaSyCnAGp4IAiDdAe2cmvhmPYg3PfBc-7Acp4",
+  authDomain: "cyberrise-certificates.firebaseapp.com",
+  databaseURL: "https://cyberrise-certificates-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "cyberrise-certificates",
+  storageBucket: "cyberrise-certificates.firebasestorage.app",
+  messagingSenderId: "358741421806",
+  appId: "1:358741421806:web:5c9105e3ebc85895780dff"
 };
 
-const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
-export { doc, getDoc, setDoc, collection, addDoc };
+// Init Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
